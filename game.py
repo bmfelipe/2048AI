fe966c915878e22be100c4b508cac47c184427e3
bmfelipe <felipebarbosamartin@gmail.com>
2020-10-27 19:15:48 +0100

    Board initialize

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import tkinter as tk #GUI
 import colors as colors 
+import random
+import numpy as np 
 
 class Game(tk.Frame):
     def __init__(self):
@@ -11,6 +13,7 @@ class Game(tk.Frame):
         )
         self.main_grid.grid(pady=(100,0))
         self.myGUI()
+        self.launch()
 
 
 
@@ -42,7 +45,35 @@ class Game(tk.Frame):
         tk.Label(score_f, text="Score", font=colors.SCORE_LABEL_FONT).grid(row=0)
         self.score_l = tk.Label(score_f, text="0", font=colors.SCORE_LABEL_FONT)
         self.score_l.grid(row=1)
+
     
+    def launch(self):
+        #we need to store data in a matrix
+        self.matrix = np.zeros((4*4), dtype="int")
+        twos_pos = np.random.default_rng().choice(16, 2, replace=False)
+        self.matrix[twos_pos] = 2
+        self.matrix = self.matrix.reshape((4,4))
+ 
+
+        #start the game with initial twos
+        # row = random.randint(0,3) row position
+        # col = random.randint(0,3) column position
+        # self.matrix[row,col] = 2
+        result = np.where(self.matrix == 2)
+        row = int(result[0][0])
+        col = int(result[1][0])
+        self.cells[row][col]["frame"].configure(bg=colors.TILE_COLORS[2])
+        self.cells[row][col]["number"].configure(bg=colors.TILE_COLORS[2],fg=colors.NUMBERS_COLORS[2],font=colors.LABEL_FONT,text="2")
+
+        row = int(result[0][1])
+        col = int(result[1][1])
+        self.cells[row][col]["frame"].configure(bg=colors.TILE_COLORS[2])
+        self.cells[row][col]["number"].configure(bg=colors.TILE_COLORS[2],fg=colors.NUMBERS_COLORS[2],font=colors.LABEL_FONT,text="2")
+
+
+        self.score=0
+
+
 
 
 Game()
