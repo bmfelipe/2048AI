3048c16aec0bf2967c0adf2491fefed8a4a29f33
bmfelipe <felipebarbosamartin@gmail.com>
2020-10-27 15:28:53 +0100

    Start showing window

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
-import tkinter as tk #GUI 
+import tkinter as tk #GUI
+import colors as colors 
 
 class Game(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
         self.grid()
         self.master.title("2048")
+        self.mainloop()
 
 
+gamegrid = Game()
